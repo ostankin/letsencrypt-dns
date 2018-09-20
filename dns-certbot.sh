4aa3901ab77d33abef8faef82f47f4e9a09e5c0c
Jay Carlson <nop@nop.com>
2018-09-20 18:21:11 -0400

    Support wildcards

--- dns-certbot.sh
+++ dns-certbot.sh
@@ -17,6 +17,12 @@ fi
 if [[ "${CERTBOT_DELEGATED:-"False"}" != "False" ]]; then
  sed -i 's|lexicon \$PROVIDER|lexicon --delegated="${CERTBOT_DELEGATED}" $PROVIDER|g' "$DEHYDRATED_HOOK"
 fi
+
+CERTBOT_ALIAS_ARG=
+if [ -n ${CERTBOT_ALIAS} ]; then
+  CERTBOT_ALIAS_ARG="--alias $CERTBOT_ALIAS"
+fi
+
 shopt -u nocasematch
 
 echo "Config fragment:"
@@ -24,7 +30,7 @@ grep -A2 "# Default values" "$DEHYDRATED_EXECUTABLE"
 
 while :;
 do
-  "$DEHYDRATED_EXECUTABLE" --cron --accept-terms --hook "$DEHYDRATED_HOOK" --challenge dns-01 --domain "$CERTBOT_DOMAIN" --out "$OUTPUT_LOCATION"
+    "$DEHYDRATED_EXECUTABLE" --cron --accept-terms --hook "$DEHYDRATED_HOOK" --challenge dns-01 --domain "$CERTBOT_DOMAIN" --out "$OUTPUT_LOCATION" $CERTBOT_ALIAS_ARG
   echo "Sleeping for $SLEEP_TIME until next check."
   sleep "$SLEEP_TIME"
 done
